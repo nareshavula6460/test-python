c55b8fd9790573fed56ad2a81918ae7ea75dfd13
nareshavula6460 <nareshavula6460@gmail.com>
2021-07-03 11:12:32 +0530

    added display program

--- DisplaySeries.py
+++ DisplaySeries.py
@@ -1,9 +1,20 @@
 n = int(input("Enter the number:"))
 sum = 0
 lst = []
+count = 1;
+
 for i in range(1,n+1):
     if i % 2 != 0:
-        
+      sum = sum + i
+      count = count + 1
+      lst.append(i)
+print(sum)
+print(lst)
+
+
+
+
+
 
 
 
